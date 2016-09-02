c78e2b583f91ab48de77f29381bf281f5cb4c44a
Blak3Nick <blaken2021@gmail.com>
2016-09-02 13:02:30 -0400

    added new recipe component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+
 })
 export class RecipesComponent implements OnInit {
 
