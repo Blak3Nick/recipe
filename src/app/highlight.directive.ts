b368e6a554bf6ab9db5be305569af100d883ac47
Blak3Nick <blaken2021@gmail.com>
2016-09-05 12:39:11 -0400

    Working with custom directives

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,26 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
-
+   @HostListener('mouseenter') mouseover(){
+     this.backgroundColor = 'green';
+   };
+  @HostListener('mouseleave') mouseleave(){
+    this.backgroundColor = 'white';
+  };
+   @HostBinding('style.backgroundColor') get setColor() {
+     return this.backgroundColor;
+   };
+   @HostListener('click', ['$event'])
+    onClick(event) {
+     console.log('Success' + event.target);
+   }
+   private backgroundColor = 'white';
   constructor(private elementREf: ElementRef, private renderer: Renderer) {
     //this.elementREf.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementREf.nativeElement, 'background-color', 'blue');
+    //this.renderer.setElementStyle(this.elementREf.nativeElement, 'background-color', 'blue');
   }
 
 }
