d4397ae8462cc8fd16c2ae7bef1fd1d9411f8e2c
Blak3Nick <blaken2021@gmail.com>
2016-09-05 13:04:53 -0400

    Working with *ngIf

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'rb-directives',
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
-export class DirectivesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DirectivesComponent  {
+  private switch = true;
+  onSwitch() {
+    this.switch = !this.switch;
   }
 
 }
