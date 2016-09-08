36b17573142c105da68ac353a808840da228ea96
Blak3Nick <blaken2021@gmail.com>
2016-09-08 13:43:22 -0400

    working with service in Ang 2

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from '../log.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
-  templateUrl: './shopping-list-add.component.html'
+  templateUrl: './shopping-list-add.component.html',
+  providers: [LogService]
 })
 export class ShoppingListAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logService: LogService ) { }
 
+  onLog(value: string) {
+    this.logService.writeToLog(value);
+  }
   ngOnInit() {
   }
 
