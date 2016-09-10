f1dae23bb97f14b39204c0b1e315cd64e352f820
Blak3Nick <blaken2021@gmail.com>
2016-09-10 15:06:31 -0400

    added new shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from "../ingredient";
 import {DataService} from "../data.service";
+import {ShoppingListService} from "../shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list',
@@ -9,10 +10,10 @@ import {DataService} from "../data.service";
 export class ShoppingListComponent implements OnInit {
   value = '';
   items: Ingredient[] = [];
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
   private dataService: DataService;
   ngOnInit() {
-
+    this.items = this.sls.getItems();
   }
 
 }
